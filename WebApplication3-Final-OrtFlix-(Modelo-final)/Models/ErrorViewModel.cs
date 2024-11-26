9b8acbeffe6af50c5627a2ac5285dc6967946968
Shelo y Papa <Shelo y Papa@DESKTOP-6BCD8AI>
2024-11-26 20:51:11 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication3-Final-OrtFlix-(Modelo-final)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication3_Final_OrtFlix__Modelo_final_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplication3-Final-OrtFlix-(Modelo-final)/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebApplication3_Final_OrtFlix__Modelo_final_.Models
+{
+    public class Usuario
+    {
+    }
+}
